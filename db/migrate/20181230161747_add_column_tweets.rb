6a31e2ab59226eef9c2ad9a103bccbcde9f93dc9
kantafujimori <fujimorikanta@fujimorikantanoMacBook-ea.local>
2018-12-31 18:56:50 +0900

    Add tweet function
    Tweetの投稿機能、消去機能、編集機能の追加。

---
+++ db/migrate/20181230161747_add_column_tweets.rb
@@ -0,0 +1,5 @@
+class AddColumnTweets < ActiveRecord::Migration[5.0]
+  def change
+    add_column :tweets, :user_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181230153451) do
+ActiveRecord::Schema.define(version: 20181230161747) do
 
   create_table "tweets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text     "text",       limit: 65535, null: false
     t.string   "image"
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
