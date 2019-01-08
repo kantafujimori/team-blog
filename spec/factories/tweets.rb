455dc4922e8391902118a57cd14347b0e956c088
kantafujimori <fujimorikanta@fujimorikantanoMacBook-ea.local>
2019-01-08 17:47:07 +0900

    Add images

--- spec/factories/tweets.rb
+++ spec/factories/tweets.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :tweet do
     text Faker::Lorem.sentence
-    image File.open("#{Rails.root}/public/image/IMG_2257.JPG")
+    image File.open("#{Rails.root}/app/image/IMG_2257.JPG")
     user
   end
 end
